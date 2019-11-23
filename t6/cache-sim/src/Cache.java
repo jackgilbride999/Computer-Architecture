eecc52e1cee9663b9543da840b9b8f09e98ba1b5
Jack Gilbride <jackgilbride999@gmail.com>
2019-11-23 22:55:33 +0000

    More functions added to Java implementation

--- t6/cache-sim/src/Cache.java
+++ t6/cache-sim/src/Cache.java
@@ -13,8 +13,8 @@ public class Cache {
 		this.L = L;
 		this.numSetBits = getNumSetBits();
 		this.sets = new Set[N];
-		for(Set set : this.sets) {
-			set = new Set(this.K, this.L);
+		for(int n=0; n<N; n++) {
+			sets[n] = new Set(this.K, this.L);
 		}
 	}
 	
@@ -29,5 +29,32 @@ public class Cache {
 		default:
 			return 0;
 		}
+	}
+	
+	int getOffset(String address) {
+		return address.charAt(0)-'0';
+	}
+	
+	int getSet(String address) {
+		int toMask = address.charAt(1) - '0';
+		return toMask >>> (8-this.numSetBits);
+	}
+	
+	String getTag(String address) {
+		char mostSignificantBits = address.charAt(1);
+		switch(this.numSetBits) {
+		case 1:
+			mostSignificantBits = (char) (mostSignificantBits & (128)); break;
+		case 2:
+			mostSignificantBits = (char) (mostSignificantBits & 192); break;
+		case 3:
+			mostSignificantBits = (char) (mostSignificantBits & 224); break;
+		}
+		return "" + mostSignificantBits + address.charAt(2) + address.charAt(3);	
+	}
+	
+	int accessAddress(String address) {
+		
+		return 0;
 	}
 }
